2353f5d83736215f9fa1fdbc98e2a0c6840b10a8
slprasanna <84315300+slprasanna2003@users.noreply.github.com>
2022-11-17 16:44:07 +0530

    Update dev1.java
    first change

--- src/csedev1/dev1.java
+++ src/csedev1/dev1.java
@@ -5,6 +5,7 @@ public class dev1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to devops");
+		System.out.println("Welcome to devops");
 	}
 
 }
